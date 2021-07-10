b535480195c95cdf9df814966c0f3dbfc32ece5f
mudomudo <irock052@gmail.com>
2021-07-10 13:38:11 +0300

    Я хз, что я написал, но это вроде даже работает

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-i = 0
-# Просто тестирую коммиты. PS Так выглядит коммент. Они нужны, чтобы объяснять другим, что ты сделал и зачем.
-# Пока не разобрался. Вроде тут должны быть комменты со ссылками куда-то. В джаве так можно.
-while i < 10:
-    print("Здорова, бандиты")
-    i = i + 1
-# Кст, что забавно, питон ругается на отсутствие табуляции в 4 и 5 строчках, когда в других языках на это насрать.
+import pygame
+import sys
+
+# Описание окна игры
+pygame.init()
+sizeBlock = 100
+margin = 15
+width = height = sizeBlock*3 + margin*4
+
+# Подключение размеров окна к библиотеке pygame
+sizeWindow = (width,height)
+screen = pygame.display.set_mode(sizeWindow)
+pygame.display.set_caption("Крестики нолики")
+
+# Описание цветов
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+white = (255, 255, 255)
+mas = [[0]*3 for i in range(3)]
+
+# Обработчик событий
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.quit():
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN():
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            col = x_mouse // (sizeBlock + margin)
+            row = y_mouse // (sizeBlock + margin)
+
+
+
+    for row in range(3):
+        for col in range(3):
+            x = col*sizeBlock + (col + 1)*margin
+            y = row*sizeBlock + (row + 1)*margin
+            pygame.draw.rect(screen, white, (x, y, sizeBlock, sizeBlock))
+    pygame.display.update()
+
